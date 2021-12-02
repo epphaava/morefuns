d72dec238b13d73b36c0b90ccde9d0b30290cf5a
Hendrik Parik <hendrik.parik@gmail.com>
2021-12-02 11:37:21 +0200

    make pictures render

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const app = express();
 //register view engine
 app.set('view engine', 'ejs');
 
-app.listen(5000);
+app.listen(3000);
 
 // public/css to static
 app.use(express.static('public/css'));
+app.use(express.static('public/assets'));
+
 
 
 app.get('/', (req, res) => {
@@ -17,24 +19,24 @@ app.get('/', (req, res) => {
         {
             user: "Juku",
             date: "18.01.2021",
-            profile_pic_src: "./public/assets/user.png",
-            post_pic_src: "./public/assets/moon.jpg",
+            profile_pic_src: "./user.png",
+            post_pic_src: "./moon.jpg",
             post_title: "lähen kooli !",
             likes: 0
         },
         {
             user: "Juku",
             date: "17.01.2021",
-            profile_pic_src: "./public/assets/user.png",
-            post_pic_src: "./assets/yoda.jpeg",
+            profile_pic_src: "./user.png",
+            post_pic_src: "./yoda.jpeg",
             post_title: "lähen kooli !",
             likes: 0
         }, 
         {
             user: "Aadu",
             date: "16.01.2021",
-            profile_pic_src: "./public/assets/user.png",
-            post_pic_src: "./assets/news.jpg",
+            profile_pic_src: "./user.png",
+            post_pic_src: "./news.jpg",
             post_title: "oi kui põnev!",
             likes: 0
         }
