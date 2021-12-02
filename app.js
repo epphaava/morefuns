68fadc165cecc70d99e390ce84c4fbc019c796ce
Epp Haavasalu <epp.haavasalu@gmail.com>
2021-12-02 11:38:37 +0200

    database babyy

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const express = require('express');
+const pool = require('./database');
+
 
 const app = express();
 
 //register view engine
 app.set('view engine', 'ejs');
 
-app.listen(5000);
+app.listen(3000);
 
 // public/css to static
 app.use(express.static('public/css'));
 
+app.get('/posts', async(req, res) => {
+    try {
+    console.log("get posts request has arrived");
+    const posts = await pool.query(
+    "SELECT * FROM posts"
+    );
+    res.json(posts.rows);
+    } catch (err) {
+    console.error(err.message);
+    }
+   });
+
+   app.get('/posts/:id', async(req, res) => {
+    try {
+    console.log("get a post request has arrived");
+    const posts = await pool.query(
+    "SELECT * FROM posts WHERE id = $1", [id]
+    );
+    res.json(posts.rows[0]);
+    } catch (err) {
+    console.error(err.message);
+    }
+   });
+
+   /*
 
 app.get('/', (req, res) => {
 
@@ -41,6 +68,7 @@ app.get('/', (req, res) => {
     ]
     res.render('index', {title: 'Home', posts});
 });
+*/
 
 app.get('/singlepost', (req, res) => {
     res.render('singlepost', {title: 'Post'});
